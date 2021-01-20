d1babf0b1a1cafd0726fc59cf05ae1d39299c004
Yoko Kasai <ykookaasi@gmail.com>
2021-01-20 16:12:35 +0100

    Fix text in test file

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,7 +51,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email addresses should be unique" do
-    duplicate_user = @user.dup
+    duplicate_user = @user.dupra a
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
