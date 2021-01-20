f360cd5bcf8bf9f716f1f069b32dcafebf3d28c1
Yoko Kasai <ykookaasi@gmail.com>
2021-01-20 14:23:55 +0100

    Add full title helper test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    # verifies that there are two such links
+    assert_select "a[href=?]", root_path, count: 2
+    # <a href="/help">...</a>
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
